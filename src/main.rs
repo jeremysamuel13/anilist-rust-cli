4b8c273f372dfc82c069c4f6173dbb96795f6f60
jeremysamuel13 <70209502+jeremysamuel13@users.noreply.github.com>
2021-08-28 20:35:22 -0400

    updated interface. prints cover picture of the queried media and added emojis to some prompts/error messages

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use std::{
-    io::{self, Write},
-};
-
-// This example uses 3 crates serde_json, reqwest, tokio
+use image::DynamicImage;
 use reqwest::{Client, Error};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
+use std::{
+    fmt::Display,
+    io::{self, Write},
+};
+use viuer::{print, Config};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AnilistEntry {
@@ -32,6 +33,9 @@ pub struct Media {
 
     #[serde(rename = "genres")]
     pub genres: Option<Vec<String>>,
+
+    #[serde(rename = "coverImage")]
+    pub coverImage: Option<CoverImage>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -46,6 +50,12 @@ pub struct Title {
     pub native: Option<String>,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CoverImage {
+    #[serde(rename = "medium")]
+    pub img: Option<String>,
+}
+
 const QUERY: &str = "
 query ($id: Int){
     Media (id: $id) {
@@ -57,11 +67,12 @@ query ($id: Int){
       }
       format
       genres
+      coverImage {
+        medium
+      }
     }
-  }
+}
 ";
-
-
 
 pub struct AnilistClient {
     client: Client,
@@ -75,11 +86,11 @@ impl AnilistClient {
     }
 
     async fn get_entry(&self, id: u32) -> AnilistEntry {
-        let client = Client::new();
         // Define query and variables
         let json = json!({"query": QUERY, "variables": {"id": id}});
         // Make HTTP post request
-        let resp = client
+        let resp = self
+            .client
             .post("https://graphql.anilist.co/")
             .header("Content-Type", "application/json")
             .header("Accept", "application/json")
@@ -98,15 +109,75 @@ impl AnilistClient {
     }
 }
 
+impl AnilistEntry {
+    async fn get_image(url: String) -> Option<DynamicImage> {
+        let img_bytes = reqwest::get(url).await.ok()?.bytes().await.ok()?;
+
+        let image = image::load_from_memory(&img_bytes).ok()?;
+
+        Some(image)
+    }
+
+    fn option_to_string<T>(opt: Option<T>) -> String
+    where
+        T: Display,
+    {
+        if let Some(x) = opt {
+            format!("{}", x)
+        } else {
+            String::from("None")
+        }
+    }
+
+    async fn print_entry(&self) {
+        if let Some(x) = &self.data.media {
+            if let Some(y) = &x.coverImage {
+                if let Some(z) = &y.img {
+                    let url = z.to_string();
+                    if let Some(img) = AnilistEntry::get_image(url).await {
+                        let conf = Config {
+                            transparent: true,
+                            absolute_offset: false,
+                            ..Default::default()
+                        };
+
+                        print(&img, &conf);
+
+                        println!(
+                            "\nID: {}\nName: {}",
+                            AnilistEntry::option_to_string(x.id),
+                            AnilistEntry::option_to_string(
+                                x.title.as_ref().unwrap().english.clone()
+                            )
+                        )
+                    }
+                }
+            }
+        } else {
+            println!("🚫 Can't print query 🚫")
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let client = AnilistClient::new();
-    println!("Connected to AniList!");
+    println!(
+        "\n
+             _______________________
+            |                       |
+            | Connected to AniList! |
+            |_______________________|
+
+            \n
+    "
+    );
 
     loop {
-        let inp = input("Enter the id: ");
+        let inp = input("🔵 Enter the id: ");
         match &inp.trim().to_lowercase()[..] {
             "exit" => {
+                println!("See you later! 👋");
                 std::process::exit(1);
             }
 
@@ -115,16 +186,18 @@ async fn main() -> Result<(), Error> {
 
                 match parse {
                     Err(_) => {
-                        println!("Invalid input");
+                        println!("🚫 Invalid input 🚫");
                         continue;
                     }
                     Ok(val) => {
                         let res = client.get_entry(val).await;
-                        println!("{:?}", &res);
+                        res.print_entry().await;
                     }
                 }
             }
         }
+
+        println!("\n----------------------------------------------------------------------------------------\n");
     }
 }
 
@@ -134,6 +207,11 @@ fn input(message: &str) -> String {
     let mut ret = String::new();
     io::stdin()
         .read_line(&mut ret)
+        .and_then(|e| {
+            println!("");
+            Ok(())
+        })
         .expect("Failed to read from stdin");
+
     ret
 }
